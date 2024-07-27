c0bf1e303c051e09c2aaebf7c205f9dad318e42b
Alireza Akbarzadeh <Akbarzadeh.Ali@hasti.co>
2024-07-27 16:59:11 +0330

    feat: implement navigation

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,5 +1,48 @@
 import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import { NavItems } from "./NavItems";
 
 export const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image
+            priority
+            src="/assets/icons/menu.svg"
+            style={{ width: "auto", height: "auto" }}
+            width={24}
+            alt="menu"
+            height={24}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <div className="w-28">
+            <Image
+              priority
+              src="/assets/images/logo.svg"
+              style={{ width: "auto", height: "auto" }}
+              width={128}
+              alt="menu"
+              height={38}
+              className="cursor-pointer"
+            />
+          </div>
+          <Separator className="border border-gray-50" />
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
 };
--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,5 +1,28 @@
+"use client";
+import { headerLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export const NavItems = () => {
-  return <div>NavItems</div>;
+  const pathname = usePathname();
+  const activePathName = (link: string) => link === pathname;
+
+  return (
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((header) => (
+        <li key={header.label}>
+          <Link
+            className={cn("flex-center p-medium-16 whitespace-nowrap", {
+              "text-primary-500 font-bold": activePathName(header.route),
+            })}
+            href={header.route}
+          >
+            {header.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
